fdec8d42010627282f231e813e3f767afc05634d
Eddy Rodriguez <e.rodriguezd@pucp.edu.pe>
2020-05-04 08:20:17 -0500

    Matematica creado y 4 funciones nombradas

---
+++ src/main/java/lab1/Matematica.java
@@ -0,0 +1,21 @@
+package lab1;
+
+public class Matematica {
+    
+    public static boolean numEsPrimo(int num){
+        return false;
+    }
+    
+    public static int mcd (int num1, int num2){
+        return 0;
+    }
+    
+    public static void fibonacci (int num){
+        
+    }
+    
+    public static int factorial(int num){
+        return 0;
+    }
+    
+}
