2baf5fded40f2a0251be375f68a9c2de99ef9c14
Eddy Rodriguez <e.rodriguezd@pucp.edu.pe>
2020-05-04 08:39:57 -0500

    numEsPrimo y mcd

--- src/main/java/lab1/Matematica.java
+++ src/main/java/lab1/Matematica.java
@@ -3,11 +3,33 @@ package lab1;
 public class Matematica {
     
     public static boolean numEsPrimo(int num){
-        return false;
+        int div = 2;
+        
+        while (div!=num){
+            if (num % div == 0){
+                return false;
+            }
+          div++;
+        }
+        return true;
     }
     
     public static int mcd (int num1, int num2){
-        return 0;
+        
+        int resto;
+        
+        do
+        {            
+            resto = num1 % num2;
+            num1 = num2;
+            
+            if (resto != 0){
+                num2 = resto;    
+            }
+        } while (resto != 0);
+
+        return num2;
+        
     }
     
     public static void fibonacci (int num){
