4de7d3d8694d19bc42d544a832140792f8967b84
Alejandro Sotomayor <asotomayorl@pucp.pe>
2020-05-04 08:56:00 -0500

    Se puso la logica de fibonacci y factorial

--- src/main/java/lab1/Matematica.java
+++ src/main/java/lab1/Matematica.java
@@ -11,10 +11,27 @@ public class Matematica {
     }
     
     public static void fibonacci (int num){
-        
+        int t1 = 0, t2 = 1;
+        //System.out.print("Serie fibonacci de " + n + " terminos: ");
+
+        for (int i = 1; i <= num; ++i)
+        {
+           // System.out.print(t1 + " ");
+
+            int sum = t1 + t2;
+            t1 = t2;
+            t2 = sum;
+        }
     }
     
     public static int factorial(int num){
+        long factorial = 1;
+        for(int i = 1; i <= num; ++i)
+        {
+            // factorial = factorial * i;
+            factorial *= i;
+        }
+        //System.out.printf("Factorial de %d = %d", num, factorial);
         return 0;
     }
     
